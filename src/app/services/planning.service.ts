cd792e1dd6532adf466b3a3d8e86d7c783b00354
natisenissay <yassine.tmnt@gmail.com>
2025-04-14 18:51:35 +0200

    Create planning.service.ts

---
+++ src/app/services/planning.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Planning } from '../models/planning.model';  // Importer le modèle Planning
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlanningService {
+
+  constructor(private http: HttpClient) {}
+
+  // Méthode pour obtenir les plannings
+  getAllPlannings(): Observable<Planning[]> {
+    return this.http.get<Planning[]>('/api/plannings');
+  }
+
+  // Méthode pour ajouter une tâche au planning
+  addPlanning(planning: Planning): Observable<Planning> {
+    return this.http.post<Planning>('/api/plannings', planning);
+  }
+}
