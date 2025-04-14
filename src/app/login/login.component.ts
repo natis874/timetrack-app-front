2dff5f4658216b3a4a0b621d6b530cd164b62ab1
natisenissay <yassine.tmnt@gmail.com>
2025-04-14 18:51:55 +0200

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { User } from '../models/user.model';  // Importer le modèle User
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (user) => {
+        if (user) {
+          this.router.navigate(['/dashboard']);
+        }
+      },
+      error: (err) => {
+        this.errorMessage = 'Nom d\'utilisateur ou mot de passe incorrect';
+      }
+    });
+  }
 }
