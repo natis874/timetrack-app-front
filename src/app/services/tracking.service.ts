5aef64041bef6fe6652fb2276fb617ed29d2c0b3
natisenissay <yassine.tmnt@gmail.com>
2025-04-14 18:51:39 +0200

    Create tracking.service.ts

---
+++ src/app/services/tracking.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tracking } from '../models/tracking.model';  // Importer le modèle Tracking
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrackingService {
+
+  constructor(private http: HttpClient) {}
+
+  // Méthode pour récupérer tous les pointages
+  getAllTrackings(): Observable<Tracking[]> {
+    return this.http.get<Tracking[]>('/api/trackings');
+  }
+
+  // Méthode pour enregistrer un nouveau pointage
+  addTracking(tracking: Tracking): Observable<Tracking> {
+    return this.http.post<Tracking>('/api/trackings', tracking);
+  }
+}
