28597c81c41da17712a184b9e170468d74324316
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-17 22:36:57 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,9 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule], 
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  imports: [RouterModule],
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'timetrack'; // ou tout autre titre que tu veux
-}
+export class AppComponent {}
