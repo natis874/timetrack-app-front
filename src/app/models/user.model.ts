ecb1a2e11781f370fabc680cfecb35f0612cde12
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-17 22:37:54 +0200

    Delete user.model.ts

--- src/app/models/user.model.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: number;
-    username: string;
-    password: string;
-    email: string;
-    role: string;   // Par exemple : "admin", "user"
-    isActive: boolean;  // Statut de l'utilisateur
-  }
