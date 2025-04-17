20dd3a43822d770b1f8e6f92c72a08faf3635a5e
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-17 22:37:50 +0200

    Delete tracking.model.ts

--- src/app/models/tracking.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Tracking {
-    id: number;
-    userId: number;  // Référence à l'utilisateur qui a effectué le pointage
-    date: string;     // Date du pointage
-    timeIn: string;   // Heure d'entrée
-    timeOut: string;  // Heure de sortie
-    hoursWorked: number;  // Nombre d'heures travaillées
-  }
