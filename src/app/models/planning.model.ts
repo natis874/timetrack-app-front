03e20b7cf0fea97e2bade534e5358a61730132d3
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-17 22:37:47 +0200

    Delete planning.model.ts

--- src/app/models/planning.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Planning {
-    id: number;
-    userId: number;  // Référence à l'utilisateur
-    date: string;    // Date de la tâche
-    task: string;    // Description de la tâche ou du planning
-    status: string;  // Statut de la tâche : "à faire", "en cours", "terminé"
-  }
