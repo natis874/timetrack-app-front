f588ad58abe64a4ea1eb259df8179936193c9af3
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-17 22:37:42 +0200

    Delete leave-request.model.ts

--- src/app/models/leave-request.model.ts
+++
@@ -1,8 +0,0 @@
-export interface LeaveRequest {
-    id: number;
-    userId: number;  // Référence à l'utilisateur
-    startDate: string;  // Date de début
-    endDate: string;    // Date de fin
-    status: string;     // Statut de la demande : "approuvé", "en attente"
-    type: string;       // Type de congé : "vacances", "maladie", etc.
-  }
