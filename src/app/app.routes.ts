560b4020a53bc908fc51e65f81f03e7efcebde11
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-17 22:37:00 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
+
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PointageComponent } from './pointage/pointage.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-];
+  { path: 'login', component: LoginComponent },
+  { path: 'pointage', component: PointageComponent },
+    { path: '**', redirectTo: 'login' }]
