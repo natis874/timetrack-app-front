e50b27736b78317a52bb17e33a5da0902763d4dc
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-17 22:38:00 +0200

    Update pointage.component.ts

--- src/app/pointage/pointage.component.ts
+++ src/app/pointage/pointage.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { PointageService } from '../core/services/pointage.service'; // Import du service
+import { Pointage } from '../core/models/pointage.model'; // Import du modèle
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pointage',
-  imports: [],
+  standalone: true, // Indique que ce composant est autonome
   templateUrl: './pointage.component.html',
-  styleUrl: './pointage.component.css'
+  styleUrls: ['./pointage.component.css'],
+  imports: [FormsModule,CommonModule] // Assure-toi que FormsModule est bien importé
 })
 export class PointageComponent {
+  entryTime: string = '';
+  exitTime: string = '';
+  pointages: Pointage[] = [];
 
+  constructor(private pointageService: PointageService) {}
+
+  // Ajouter un pointage
+  addPointage() {
+    if (this.entryTime && this.exitTime) {
+      this.pointageService.addPointage(this.entryTime, this.exitTime);
+      this.pointages = this.pointageService.getPointages(); // Mettre à jour la liste des pointages
+      this.entryTime = '';
+      this.exitTime = '';
+    }
+  }
 }
