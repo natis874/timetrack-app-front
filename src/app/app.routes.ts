67f2cdbc27093e9c50d4bb0fe9c919f7f2a429ae
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-18 18:54:06 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
-
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PointageComponent } from './pointage/pointage.component';
+import { AuthGuard } from './core/guard/auth.guard';
+import { RoleGuard } from './core/guard/role.guard';
+import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'pointage', component: PointageComponent },
-    { path: '**', redirectTo: 'login' }]
+  { path: '', component: LoginComponent },
+  {
+    path: 'pointage',
+    component: PointageComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard, RoleGuard('manager')],
+  },
+  { path: '**', redirectTo: '' },
+];
