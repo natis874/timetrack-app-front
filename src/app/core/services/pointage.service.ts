83b672099f78b53fab92f84d1b46ad6d43a1e749
natisenissay <72911307+natis874@users.noreply.github.com>
2025-04-18 18:54:35 +0200

    Update pointage.service.ts

--- src/app/core/services/pointage.service.ts
+++ src/app/core/services/pointage.service.ts
@@ -1,41 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Pointage } from '../models/pointage.model';
 
-
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root',
+})
 export class PointageService {
-  private pointages: Pointage[] = [];
-  private idCounter = 1;
+  private lastPointage: Pointage | null = null;
 
-  constructor() {
-    const saved = localStorage.getItem('pointages');
-    this.pointages = saved ? JSON.parse(saved) : [];
-    this.idCounter = this.pointages.length + 1;
-  }
-  
-
-  addPointage(entryTime: string, exitTime: string): void {
-    const totalHours = this.calculateHours(entryTime, exitTime);
-    const pointage = {
-      id: this.idCounter++,
-      entryTime,
-      exitTime,
-      totalHours,
-    };
-    this.pointages.push(pointage);
-    localStorage.setItem('pointages', JSON.stringify(this.pointages));
+  pointer(type: 'entrée' | 'sortie') {
+    const now = new Date().toLocaleString();
+    this.lastPointage = { type, time: now };
   }
 
-  getPointages(): Pointage[] {
-    return this.pointages;
-  }
-
-
-  
-  private calculateHours(entry: string, exit: string): number {
-    const start = new Date(entry).getTime();
-    const end = new Date(exit).getTime();
-    return Math.round((end - start) / (1000 * 60 * 60) * 100) / 100;
+  getLastPointage(): Pointage | null {
+    return this.lastPointage;
   }
 }
-
